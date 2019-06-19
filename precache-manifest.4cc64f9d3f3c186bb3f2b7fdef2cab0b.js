b95d22e0b2c274cde026861b15454b5b04a9142f
Nyan <an.muploader@gmail.com>
2019-06-19 15:55:24 -0500

    Updates

---
+++ precache-manifest.4cc64f9d3f3c186bb3f2b7fdef2cab0b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "288760f618fb44854f2772db923cbb85",
+    "url": "/deepdaemon_react/index.html"
+  },
+  {
+    "revision": "defc10f2f43fdd145a3e",
+    "url": "/deepdaemon_react/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "f52e4d95b6c858f9f78c",
+    "url": "/deepdaemon_react/static/css/main.a5d4dbaf.chunk.css"
+  },
+  {
+    "revision": "defc10f2f43fdd145a3e",
+    "url": "/deepdaemon_react/static/js/2.93ad75bc.chunk.js"
+  },
+  {
+    "revision": "f52e4d95b6c858f9f78c",
+    "url": "/deepdaemon_react/static/js/main.c8c8ef9c.chunk.js"
+  },
+  {
+    "revision": "f98d519371d5cbee753b",
+    "url": "/deepdaemon_react/static/js/runtime~main.beae6893.js"
+  },
+  {
+    "revision": "fe08354defbb4897e4f44d83fa63917f",
+    "url": "/deepdaemon_react/static/media/header_bg.fe08354d.png"
+  },
+  {
+    "revision": "d765de791d6acd5ae8c89a425df6f17c",
+    "url": "/deepdaemon_react/static/media/icomoon-.d765de79.eot"
+  },
+  {
+    "revision": "8f5b334056407612b775b00f6fb41a70",
+    "url": "/deepdaemon_react/static/media/icomoon.8f5b3340.svg"
+  },
+  {
+    "revision": "bbec2589df6435c5f24a7e5306312e30",
+    "url": "/deepdaemon_react/static/media/icomoon.bbec2589.woff"
+  },
+  {
+    "revision": "c5f1ad61684a686c30ce63da3c190ab9",
+    "url": "/deepdaemon_react/static/media/icomoon.c5f1ad61.ttf"
+  },
+  {
+    "revision": "d765de791d6acd5ae8c89a425df6f17c",
+    "url": "/deepdaemon_react/static/media/icomoon.d765de79.eot"
+  },
+  {
+    "revision": "4e62c623ecf21c74719823881cf3a8e0",
+    "url": "/deepdaemon_react/static/media/logo.4e62c623.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/deepdaemon_react/precache-manifest.4cc64f9d3f3c186bb3f2b7fdef2cab0b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/deepdaemon_react/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
