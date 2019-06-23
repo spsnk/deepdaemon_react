f548681dff533720637a89b3f6be673c343c3c69
Salvador Paz Santos <18468817+spsnk@users.noreply.github.com>
2019-06-23 00:23:49 -0500

    Fixed warning and converted to arrow function.

--- src/components/team_card.js
+++ src/components/team_card.js
@@ -3,7 +3,7 @@ import Card from 'react-bootstrap/Card';
 import Image from 'react-bootstrap/Image'
 import './team_card.css';
 
-function Team_card ({team,sort=false,status="V"}) {
+const Team_card = ({team,sort=false,status="current"}) => {
     let sorted = Object.keys(team);
     if (sort) {
         sorted.sort();
@@ -12,25 +12,25 @@ function Team_card ({team,sort=false,status="V"}) {
         <>
             {
                 sorted.map(key => {
-                    if (team[key].status!==status)
-                    return (null);
+                    if (team[key].status===status)
+                        return (<Card key={key} className="team">
+                            <Image className="" roundedCircle src={require(`../assets/img/team/small/${team[key].photo}`)} alt={team[key].photo} />
+                            <Card.Header>
+                                <Card.Title>{team[key].name}</Card.Title>
+                                <Card.Text>
+                                    {team[key].from} <br />
+                                    <a href={team[key].linkedin} target="_blank" alt="linkedin" rel="noopener noreferrer">
+                                        <span className="fab fa-linkedin" />
+                                    </a>
+                                    <a href={"mailto:" + team[key].email} target="_blank" alt="email" rel="noopener noreferrer">
+                                        <span className="fas fa-envelope" />
+                                    </a>
+                                </Card.Text>
+                            </Card.Header>
+                            <Card.Body>{team[key].flavor}</Card.Body>
+                        </Card>);
                     else
-                    return (<Card key={key} className="team">
-                        <Image className="" roundedCircle src={require(`../assets/img/team/small/${team[key].photo}`)} alt={team[key].photo} />
-                        <Card.Header>
-                            <Card.Title>{team[key].name}</Card.Title>
-                            <Card.Text>
-                                {team[key].from} <br />
-                                <a href={team[key].linkedin} target="_blank" alt="linkedin" rel="noopener noreferrer">
-                                    <span className="fab fa-linkedin" />
-                                </a>
-                                <a href={"mailto:" + team[key].email} target="_blank" alt="email" rel="noopener noreferrer">
-                                    <span className="fas fa-envelope" />
-                                </a>
-                            </Card.Text>
-                        </Card.Header>
-                        <Card.Body>{team[key].flavor}</Card.Body>
-                    </Card>);
+                        return (null);
                 })
             }
         </>
