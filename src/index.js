b6f1006a3187268e3d4c75f348b2eb730f606e43
Salvador Paz Santos <18468817+spsnk@users.noreply.github.com>
2019-06-23 07:05:03 -0500

    * Readded Service Worker

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import DeepDaemon from './DeepDaemon.js';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'react-bootstrap/dist/react-bootstrap';
-//import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<DeepDaemon />, document.getElementById('root'));
 
-//serviceWorker.unregister();
+serviceWorker.unregister();
