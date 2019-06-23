ae05b7c49ad89a02d924f5e16883a20bedd7a5c6
Salvador Paz Santos <18468817+spsnk@users.noreply.github.com>
2019-06-23 06:57:35 -0500

    * Disabled service worker

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import DeepDaemon from './DeepDaemon.js';
-import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'react-bootstrap/dist/react-bootstrap';
+//import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<DeepDaemon />, document.getElementById('root'));
 
-serviceWorker.unregister();
+//serviceWorker.unregister();
