40af6b94da0c35125ddc1d34ebc08a0c0339bcb5
Salvador Paz Santos <18468817+spsnk@users.noreply.github.com>
2019-07-08 21:02:56 -0500

    Updated members.read to return object instead array

--- api/objects/member.php
+++ api/objects/member.php
@@ -63,7 +63,7 @@ class Member
         $stmt = $this->conn->prepare($query);
         // execute query
         $stmt->execute();
-        $this->parse($stmt);
+        $this->parse_one($stmt);
     }
 
     private function parse($stmt)
@@ -94,6 +94,50 @@ class Member
                     "school_long" => is_null($school_long) ? [] : explode(",", $school_long),
                 );
                 array_push($members_arr, $member_item);
+            }
+
+            // set response code - 200 OK
+            http_response_code(200);
+
+            // show members data in json format
+            echo json_encode($members_arr);
+        } else {
+            // set response code - 404 Not found
+            http_response_code(404);
+            // tell the user no members found
+            echo json_encode(
+                array("message" => "No members found.")
+            );
+        }
+    }
+    private function parse_one($stmt)
+    {
+        $num = $stmt->rowCount();
+        // check if more than 0 record found
+        if ($num > 0) {
+            // members array
+            $members_arr = array();
+            // retrieve our table contents
+            // fetch() is faster than fetchAll()
+            // http://stackoverflow.com/questions/2770630/pdofetchall-vs-pdofetch-in-a-loop
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                // extract row
+                // this will make $row['name'] to
+                // just $name only
+                extract($row);
+                $member_item = array(
+                    "id" => $id,
+                    "name" => $name,
+                    "linkedin" => $linkedin,
+                    "email" => $email,
+                    "short_desc" => $short_desc,
+                    "photo_filename" => $photo_filename,
+                    "career" => is_null($career) ? [] : explode(",", $career),
+                    "school" => is_null($school) ? [] : explode(",", $school),
+                    "career_long" => is_null($career_long) ? [] : explode(",", $career_long),
+                    "school_long" => is_null($school_long) ? [] : explode(",", $school_long),
+                );
+                $members_arr = $member_item;
             }
 
             // set response code - 200 OK
