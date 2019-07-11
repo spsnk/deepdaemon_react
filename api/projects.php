688134f6a0c3e3d1b40b57a92b6648034a5aa69d
Salvador Paz Santos <18468817+spsnk@users.noreply.github.com>
2019-07-11 02:43:59 -0500

    Added cache expire to headers

--- api/projects.php
+++ api/projects.php
@@ -4,6 +4,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 header("Content-Type: application/json; charset=UTF-8");
+header('Expires: '.gmdate('D, d M Y H:i:s \G\M\T', time() + (60 * 60)));
 
 // include database and util files
 include_once './config/database.php';
